b59fc64276191258f5363d2dd866bc27992b6bcd
Simon Martinelli <simon@martinelli.ch>
2025-01-01 15:12:02 +0100

    Add test for registration form submission and notification

--- src/test/java/ch/martinelli/oss/registration/ui/views/registration/PublicEventRegistrationViewTest.java
+++ src/test/java/ch/martinelli/oss/registration/ui/views/registration/PublicEventRegistrationViewTest.java
@@ -1,10 +1,17 @@
 package ch.martinelli.oss.registration.ui.views.registration;
 
 import ch.martinelli.oss.registration.ui.views.KaribuTest;
+import com.github.mvysny.kaributesting.v10.LocatorJ;
+import com.github.mvysny.kaributesting.v10.NotificationsKt;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.html.H2;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static com.github.mvysny.kaributesting.v10.LocatorJ._click;
 import static com.github.mvysny.kaributesting.v10.LocatorJ._get;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
@@ -25,5 +32,12 @@ class PublicEventRegistrationViewTest extends KaribuTest {
 
         H2 title = _get(H2.class, spec -> spec.withText("Lane Eula"));
         assertThat(title).isNotNull();
+
+        List<Checkbox> checkboxes = LocatorJ._find(Checkbox.class);
+        checkboxes.forEach(checkbox -> _click(checkbox));
+
+        _click(_get(Button.class, spec -> spec.withText("Anmelden")));
+
+        NotificationsKt.expectNotifications("Vielen Dank für die Anmeldung!");
     }
 }
