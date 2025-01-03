7e734722f3b0977848bd1374f3e556af963f214a
Simon Martinelli <simon@martinelli.ch>
2025-01-03 14:09:05 +0100

    Use Mailcatcher during development

--- src/test/java/ch/martinelli/oss/registration/TestcontainersConfiguration.java
+++ src/test/java/ch/martinelli/oss/registration/TestcontainersConfiguration.java
@@ -3,8 +3,11 @@ package ch.martinelli.oss.registration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
+import skydrinker.testcontainers.mailcatcher.MailCatcherContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestcontainersConfiguration {
@@ -15,4 +18,18 @@ public class TestcontainersConfiguration {
         return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16.1"));
     }
 
+    @Bean
+    MailCatcherContainer mailcatcherContainer() {
+        MailCatcherContainer mailCatcherContainer = new MailCatcherContainer();
+        mailCatcherContainer.start();
+        return mailCatcherContainer;
+    }
+
+    @Bean
+    JavaMailSender mailSender(MailCatcherContainer mailcatcherContainer) {
+        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+        sender.setHost(mailcatcherContainer.getHost());
+        sender.setPort(mailcatcherContainer.getSmtpPort());
+        return sender;
+    }
 }
