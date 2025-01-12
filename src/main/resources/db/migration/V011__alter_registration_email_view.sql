cba82f9408bed1d120e0cf88aa31a3af1133722b
Simon Martinelli <simon@martinelli.ch>
2025-01-12 09:51:15 +0100

    Revise registration email view and repository query
    Signed-off-by: Simon Martinelli <simon@martinelli.ch>

---
+++ src/main/resources/db/migration/V011__alter_registration_email_view.sql
@@ -0,0 +1,12 @@
+drop view registration_email_view;
+
+create view registration_email_view as
+select r.id  as registration_id,
+       r.year,
+       re.id as registration_email_id,
+       re.email,
+       re.link,
+       re.sent_at,
+       re.registered_at
+from registration_email re
+         join registration r on re.registration_id = r.id;
