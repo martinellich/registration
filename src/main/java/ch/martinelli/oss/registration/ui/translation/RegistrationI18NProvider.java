2b00ebeb0989c508131a8db87015adb8bc664fdd
Simon Martinelli <simon@martinelli.ch>
2025-02-11 09:58:26 +0100

    Refactor RegistrationI18NProvider for Spring component support
    Signed-off-by: Simon Martinelli <simon@martinelli.ch>

--- src/main/java/ch/martinelli/oss/registration/ui/translation/RegistrationI18NProvider.java
+++ src/main/java/ch/martinelli/oss/registration/ui/translation/RegistrationI18NProvider.java
@@ -1,20 +1,18 @@
 package ch.martinelli.oss.registration.ui.translation;
 
 import com.vaadin.flow.i18n.DefaultI18NProvider;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Locale;
 
+@Component
 public class RegistrationI18NProvider extends DefaultI18NProvider {
 
     private final static List<Locale> PROVIDED_LOCALES = List.of(Locale.ENGLISH, Locale.GERMAN);
 
-    public RegistrationI18NProvider(List<Locale> PROVIDED_LOCALES) {
+    public RegistrationI18NProvider() {
         super(PROVIDED_LOCALES);
-    }
-
-    public RegistrationI18NProvider(List<Locale> PROVIDED_LOCALES, ClassLoader classLoader) {
-        super(PROVIDED_LOCALES, classLoader);
     }
 
 }
